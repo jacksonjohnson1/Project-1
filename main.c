ecedff6d54dbe54c37e3de255b2d3c3d7d7a828e
Jackson Johnson <c3303926@uon.edu.au>
2019-04-02 03:01:56 +0000

    master: fixed bugs

--- main.c
+++ main.c
@@ -1,7 +1,16 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(){
-    
-    printf("wasup world");
-    return 0 ;
+int main()
+{
+    int q;
+    //hb
+    char letters [26] {a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z}; //{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'}; 
+    {
+        printf("Insert sentence: "); //Input a s word or sentence
+        scanf(letters[], &c); //Read the sentence to be encoded
+        c = c + 3 // implements new code
+        printf("New sentence: ")
+        for (q = 0; q < 26; q++); 
+    }
 }
